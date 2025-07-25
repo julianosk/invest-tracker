e6ea38a30509657d56267198760efe9a703927c1
Juliano Krieger <julianosk@gmail.com>
2025-07-26 00:05:52 +0200

    Convert database to postgres

--- backend/models/investment.go
+++ backend/models/investment.go
@@ -2,27 +2,25 @@ package models
 
 import (
 	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type AssetClass struct {
-    Name        string `bson:"name" json:"name"`
-    Description string `bson:"description" json:"description"`
+	Name        string `bson:"name" json:"name"`
+	Description string `bson:"description" json:"description"`
 }
 
 type AssetSubClass struct {
-    Name        string `bson:"name" json:"name"`
-    Description string `bson:"description" json:"description"`
-    AssetClass  string `bson:"asset_class" json:"asset_class"`
+	Name        string `bson:"name" json:"name"`
+	Description string `bson:"description" json:"description"`
+	AssetClass  string `bson:"asset_class" json:"asset_class"`
 }
 
 // Update Investment struct to include AssetSubClass
 type Investment struct {
-    ID            primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-    Name          string             `bson:"name" json:"name"`
-    Amount        float64            `bson:"amount" json:"amount"`
-    AssetClass    string             `bson:"asset_class" json:"asset_class"`
-    AssetSubClass string             `bson:"asset_sub_class" json:"asset_sub_class"`
-    CreatedAt     time.Time          `bson:"created_at" json:"created"`
+	ID            uint      `gorm:"primaryKey" json:"id"`
+	Name          string    `json:"name"`
+	Amount        float64   `json:"amount"`
+	AssetClass    string    `json:"asset_class"`
+	AssetSubClass string    `json:"asset_sub_class"`
+	CreatedAt     time.Time `json:"created"`
 }
